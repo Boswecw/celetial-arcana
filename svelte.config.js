02882ee1c7028f793bfe76fc87eb025fa467df98
Charles Boswell <charliewboswell@gmail.com>
2025-11-04 13:47:37 -0500

    Configure for Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,15 +7,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : '/celetial-arcana'
-		}
+			edge: false,
+			split: false
+		})
 	}
 };
 
